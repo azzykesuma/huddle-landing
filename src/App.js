96e902c2b0b83223f36b8600d41c53d1d5d587b0
Azzy Kesuma <official.azzy@gmail.com>
2021-07-01 16:37:03 +0700

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.svg';
+import mockup from './images/illustration-mockups.svg'
+import facebook from './images/facebook.png'
+import twitter from './images/twitter.png'
+import instagram from './images/instagram.svg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='container'>
+      <div className='left'>
+        <img className='logo' src={logo} alt='logo' />
+        <img className='mockup' src={mockup} alt='design' />
+      </div>
+      <div className='right'>
+        <h1>Build The Community Your Fans Will Love</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+          Create connections with your users as you engage in genuine discussion.
+       </p>
+       <button>Register</button>
+       <div className='socmedContainer'>
+          <img className='media' src={facebook} alt='facebook' />
+          <img className='media' src={twitter} alt='twitter' />
+          <img className='media' src={instagram} alt='instagram' />
+       </div>
+      </div>
     </div>
   );
 }
